36ce7c31b7bc76af47625fe9441309001b6a831f
Vivek-KA <vivekka6656@gmail.com>
2026-02-07 22:47:23 +0530

    fix: app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 import numpy as np;
 import pandas as pd;
-import matplotlib.pyplot  as plt;
+# import matplotlib.pyplot  as plt;
 from sklearn.model_selection  import train_test_split
 from sklearn.linear_model  import LogisticRegression
 from sklearn.metrics import accuracy_score,confusion_matrix
